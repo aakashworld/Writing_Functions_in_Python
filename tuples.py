3b908a499914e924bbda36dedbdd0005a811764a
Aakash Raj <aakash.raj@asu.edu>
2020-05-11 01:21:47 -0400

    Functions Part 1

---
+++ tuples.py
@@ -0,0 +1,5 @@
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+
+# Construct even_nums composed of the same elements in nums, but with the 1st element replaced with the value, 2.
+even_nums = (2, num2, num3)
